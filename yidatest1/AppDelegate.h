f1d2c771afde80670bcf933e044320e4907fff8a
张伟 <yida@qima_inc.com>
2015-10-19 12:02:54 +0800

    commit

---
+++ yidatest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yidatest1
+//
+//  Created by 益达 on 15/10/19.
+//  Copyright © 2015年 张伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yidatest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yidatest1
+//
+//  Created by 益达 on 15/10/19.
+//  Copyright © 2015年 张伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
